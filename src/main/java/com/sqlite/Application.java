6bc537fc201acfcb095bd47d33758bb0dfcc3c80
RogerChen <jjc.roger@gmail.com>
2018-01-24 20:46:21 +0800

    added default data

--- src/main/java/com/sqlite/Application.java
+++ src/main/java/com/sqlite/Application.java
@@ -1,6 +1,9 @@
 
 package com.sqlite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -10,20 +13,22 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import com.sqlite.dao.DepartmentDao;
+import com.sqlite.dao.EmployeeDao;
 import com.sqlite.dao.HabbitDao;
 import com.sqlite.entities.Department;
+import com.sqlite.entities.Employee;
 import com.sqlite.entities.Habbit;
 
 @SpringBootApplication
-public class SampleSqliteApplication {
-	private static final Logger log = LoggerFactory.getLogger(SampleSqliteApplication.class);
+public class Application {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(SampleSqliteApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner demo(DepartmentDao departmentDao, HabbitDao habbitDao) {
+	public CommandLineRunner demo(DepartmentDao departmentDao, HabbitDao habbitDao,EmployeeDao employeeDao) {
 		return (args) -> {
 			if (departmentDao.findAll().size() == 0) {
 				departmentDao.save(new Department("基金業務部"));
@@ -39,6 +44,23 @@ public class SampleSqliteApplication {
 				habbitDao.save(new Habbit("看書"));
 				habbitDao.save(new Habbit("發呆"));
 				habbitDao.save(new Habbit("寫程式"));
+			}
+			
+			if(employeeDao.findAll().size()==0) {
+				Employee employee = new Employee();
+				employee.setRocId("A12345678");
+				employee.setName("Roger");
+				Department dep =  new Department();
+				dep.setId(1);
+				employee.setEmployeeNo("A1204");
+				employee.setDepartment(dep);
+				employee.setMarriage(Boolean.FALSE);
+				employee.setChild(0);
+				List<Habbit> list = new ArrayList<Habbit>();				
+				list.add(habbitDao.getOne(1));
+				employee.setHabbits(list);
+				employeeDao.save(employee);
+				
 			}
 		};
 	}
